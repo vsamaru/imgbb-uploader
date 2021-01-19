29f38bee5ce1d51929e1fa36ae3c9b6494470750
vsamaru <vsamaru@gmail.com>
2021-01-19 04:08:57 +0200

    Update imgbb.ts

--- src/api/imgbb.ts
+++ src/api/imgbb.ts
@@ -9,5 +9,6 @@ export async function upload(apiKey: string, image: string, name: string): Promi
   })).json();
   if (!resp.success)
     throw `api upload fail: ${resp.status_code}: ${resp.error.message}`;
+  console.log(resp.data.url_viewer)
   return { url: resp.data.display_url, filename:resp.data.image.filename, thumbUrl:resp.data.thumb.url, preUrl:resp.data.url_viewer };
 }
